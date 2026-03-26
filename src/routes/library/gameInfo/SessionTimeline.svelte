c90c3634242a79681fade3d67c012eeff0a9380c
Connor <connordickie02@gmail.com>
2026-03-26 11:42:33 +0000

    added commenting

--- src/routes/library/gameInfo/SessionTimeline.svelte
+++ src/routes/library/gameInfo/SessionTimeline.svelte
@@ -1,14 +1,16 @@
 <script>
-    import { formatDate, formatTime, formatDuration} from "./timeFormatting.js";
+    import { formatDate } from "./timeFormatting.js";
     import { Timeline, TimelineItem} from "flowbite-svelte";
 
+    // Sessions prop from gameInfo page.
     let { sessions } = $props();
 </script>
 
 <Timeline>
 
+    <!--Displays the date and notes of each session for a specific game in a timeline-->
     {#each sessions as session}
-        <TimelineItem class="text-gray-300" title="Notes:" date={formatDate(session.startTs)}>{session.notes || "No session notes"}</TimelineItem>
+        <TimelineItem class="text-gray-300" title="Notes:" date={formatDate(session.startTs)} dateFormat="full-date">{session.notes || "No session notes"}</TimelineItem>
     {/each}
 
 </Timeline>
