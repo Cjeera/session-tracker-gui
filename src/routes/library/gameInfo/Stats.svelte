a574db63bcab9b8e07f5ccbb453254c330bd1c6d
Connor <connordickie02@gmail.com>
2026-03-26 14:18:54 +0000

    add bar chart to charts section

--- src/routes/library/gameInfo/Stats.svelte
+++ src/routes/library/gameInfo/Stats.svelte
@@ -1,9 +1,53 @@
 <script>
-    import { formatDate, formatTime, formatDuration} from "./timeFormatting.js";
+    import {
+        formatDate,
+        formatLocaleDate,
+        formatTime,
+        formatDuration,
+    } from "./timeFormatting.js";
     import { Chart } from "@flowbite-svelte-plugins/chart";
 
     let { sessions } = $props();
+
+    function getSessionRange(sessions) {
+        let tally = {};
+
+        for (const session of sessions) {
+            let dateStr = formatLocaleDate(session.startTs)
+
+            if (tally[dateStr]) {
+                tally[dateStr] += 1;
+            } else {
+                tally[dateStr] = 1;
+            }
+        }
+
+        let dates = Object.keys(tally).slice(-7);
+   
+        let counts = Object.values(tally).slice(-7);
+
+        return {dates, counts};
+
+    }
+
+    let chartData = $derived(getSessionRange(sessions));
+
+    /** @type {import('apexcharts').ApexOptions} */
+    let options = $derived({
+        chart: {
+            type: "bar",
+            height: "300px",
+        },
+        series: [
+            {
+                name: "Sessions",
+                data: chartData.counts,
+            },
+        ],
+        xaxis: {
+            categories: chartData.dates,
+        },
+    });
 </script>
 
-<p>TBD!</p>
-
+<Chart {options} />
